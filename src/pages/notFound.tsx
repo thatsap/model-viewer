10f0b2ba668b6e9289966dce1c3d6c9648575a17
thatsap <apandey2908@gmail.com>
2024-12-17 09:38:42 +0530

    Adds Routes for developer, user and not found

---
+++ src/pages/notFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound() {
+    return (
+      <div className="flex flex-col items-center justify-center w-full">
+        <div className="inline-block text-2xl text-red-500">404 - Not Found!</div>
+        <p>Ahan!!! Where to?</p>
+      </div>
+    );
+  }
